dadf5d2eaa9b45ce85c22755e31e416be60d5da2
shyq13 <101326128+shyq13@users.noreply.github.com>
2022-05-10 18:07:53 +0800

    添加项目文件。

--- lesson5/lesson5（3）/lesson5（3）.cpp
+++ lesson5/lesson5（3）/lesson5（3）.cpp
@@ -4,6 +4,15 @@
 //运行该项目时一定要将它设为启动项！！！
 using namespace std;
 
+enum HeroType {    //创建枚举类型
+	Warrior,
+	Mage,
+	Archer,
+	Assassin,
+	Priest,
+	ADC
+};
+
 int main() {
 	//switch语句
 	//switch语句是一个简单的选择语句，它的作用是根据一个条件来执行不同的语句。
@@ -51,6 +60,95 @@ int main() {
 		break;
 	}
 	//输出结果：字符a对应的ASCII编码是97
+
+	cout << '\n';
+	cout << "--------------------------------------------" << endl;
+	cout << '\n';
+
+	//同样，也可以使用枚举类型
+	HeroType hero = Mage;
+	switch (hero) {
+	case HeroType::Warrior:
+		cout << "我是一个战士" << endl;
+		break;
+	case HeroType::Mage:
+		cout << "我是一个法师" << endl;
+		break;
+	case HeroType::Archer:
+		cout << "我是一个弓箭手" << endl;
+		break;
+	case HeroType::Assassin:
+		cout << "我是一个刺客" << endl;
+		break;
+	case HeroType::Priest:
+		cout << "我是一个牧师" << endl;
+		break;
+	case HeroType::ADC:
+		cout << "我是一个射手" << endl;
+		break;
+	default:
+		cout << "我是一个其他英雄" << endl;
+		break;
+	}
+	//输出结果：我是一个法师
+
+	cout << '\n';
+	cout << "--------------------------------------------" << endl;
+	cout << '\n';
+	
+	int vip;  //定义一个会员等级
+	cout << "请输入您的会员等级：";
+	cin >> vip;
+	switch (vip) {
+	case 0:
+		cout << "没有登录奖励" << endl;
+		break;
+	case 1:
+		cout << "登录奖励：10个金币" << endl;
+		break;
+	case 2:
+		cout << "登录奖励：20个金币" << endl;
+		break;
+	case 3:
+		cout << "登录奖励：5个钻石" << endl;
+		break;
+	case 4:
+		cout << "登录奖励：10个钻石" << endl;
+		break;
+	default:
+		cout << "登录奖励：一天英雄试用卷" << endl;
+		break;
+	}
+	//这里也可以转换成if语句
+	
+	cout << '\n';
+	cout << "--------------------------------------------" << endl;
+	cout << '\n';
+	
+	cout << "切换为if判断" << endl << endl;
+	vip = 0;
+	cout << "请输入您的会员等级：";
+	cin >> vip;
+	if (vip == 0) {
+		cout << "没有登录奖励" << endl;
+	}
+	else if (vip == 1) {
+		cout << "登录奖励：10个金币" << endl;
+	}
+	else if (vip == 2) {
+		cout << "登录奖励：20个金币" << endl;
+	}
+	else if (vip == 3) {
+		cout << "登录奖励：5个钻石" << endl;
+	}
+	else if (vip == 4) {
+		cout << "登录奖励：10个钻石" << endl;
+	}
+	else {
+		cout << "登录奖励：一天英雄试用卷" << endl;
+	}
+	//与switch的结果一致
+	//注意！所有的switch语句都可以转化为if语句，但是有时if语句却不能转化为switch语句
 
 	
 	return 0;
